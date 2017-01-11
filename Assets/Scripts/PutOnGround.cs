c3732143e32bad2c82905a91c9a6bfd954bedc14
Jeremy Kenyon <lanthos@gmail.com>
2017-01-10 19:39:26 -0800

    Created secondary mountain type and terrain now randomly picks between the two.  Created spawn points for players 1 and 2 (not hooked up yet though).
    Also added put on ground script to ensure that the tanks stay on the
    terrain.

---
+++ Assets/Scripts/PutOnGround.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PutOnGround : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 fixedSpot = transform.position;
+		fixedSpot.y = Terrain.activeTerrain.SampleHeight(fixedSpot) + Terrain.activeTerrain.transform.position.y;
+		transform.position = fixedSpot;
+	}
+}
--- Assets/Scripts/Terrain deformation/FormTerrain.cs
+++ Assets/Scripts/Terrain deformation/FormTerrain.cs
@@ -6,7 +6,7 @@ public class FormTerrain : MonoBehaviour {
 
 	public TerrainDeformationManager tdManager;
 	public GameObject[] terrainShapes;
-//	public Transform tempPos;
+	public int terrainSpawnCount = 1;
 	public Transform[] SpawnBox;
 
 	// Use this for initialization
@@ -16,9 +16,9 @@ public class FormTerrain : MonoBehaviour {
 	
 	// Update is called once per frame
 	IEnumerator SpawnTerrain () {
-		for (int i = 0; i < 15; i++) {
+		for (int i = 0; i < terrainSpawnCount; i++) {
 			yield return new WaitForSeconds (0.5f);
-			GameObject tempGO = GameObject.Instantiate (terrainShapes [0]);
+			GameObject tempGO = GameObject.Instantiate (terrainShapes [Random.Range(0, terrainShapes.Length)]);
 			Vector3 randInSpawnBox;
 			randInSpawnBox = new Vector3 (Random.Range (-1.0f, 1.0f), Random.Range (-1.0f, 1.0f), Random.Range (-1.0f, 1.0f));
 			randInSpawnBox = SpawnBox[Random.Range(0, SpawnBox.Length)].TransformPoint (randInSpawnBox * 0.5f);
