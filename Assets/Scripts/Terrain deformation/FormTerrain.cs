89fc38b34f609595b1f1677296c2817ab5e51a43
Jeremy Kenyon <lanthos@gmail.com>
2017-01-22 00:13:08 -0800

    created separation for small, medium, and tall mountain spawn areas, prefrabs, and counts to allow for better terrain setups.

--- Assets/Scripts/Terrain deformation/FormTerrain.cs
+++ Assets/Scripts/Terrain deformation/FormTerrain.cs
@@ -5,24 +5,43 @@ using UnityEngine;
 public class FormTerrain : MonoBehaviour {
 
 	public TerrainDeformationManager tdManager;
-	public GameObject[] terrainShapes;
-	public int terrainSpawnCount = 1;
-	public Transform[] SpawnBox;
+	public GameObject[] shortTerrainShapes;
+	public GameObject[] midTerrainShapes;
+	public GameObject[] tallTerrainShapes;
+	public int shortTerrainSpawnCount = 0;
+	public int midTerrainSpawnCount = 0;
+	public int tallTerrainSpawnCount = 0;
+	public Transform[] shortSpawnBox;
+	public Transform[] midSpawnBox;
+	public Transform[] tallSpawnBox;
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine (SpawnTerrain ());
+		SpawnTerrain ();
 	}
-	
-	// Update is called once per frame
-	IEnumerator SpawnTerrain () {
-		for (int i = 0; i < terrainSpawnCount; i++) {
+
+	IEnumerator SpawnTerrainType(int terrainCount, GameObject[] terrainShapes, Transform[] spawnBoxes){
+		for (int i = 0; i < terrainCount; i++) {
 			yield return new WaitForSeconds (0.5f);
 			GameObject tempGO = GameObject.Instantiate (terrainShapes [Random.Range(0, terrainShapes.Length)]);
 			Vector3 randInSpawnBox;
 			randInSpawnBox = new Vector3 (Random.Range (-1.0f, 1.0f), Random.Range (-1.0f, 1.0f), Random.Range (-1.0f, 1.0f));
-			randInSpawnBox = SpawnBox[Random.Range(0, SpawnBox.Length)].TransformPoint (randInSpawnBox * 0.5f);
+			randInSpawnBox = spawnBoxes[Random.Range(0, spawnBoxes.Length)].TransformPoint (randInSpawnBox * 0.5f);
 			tdManager.ApplyDeform (tempGO.GetComponent<TerrainDeformer>(), randInSpawnBox);
 		}
+	}
+	
+	// Update is called once per frame
+	void SpawnTerrain () {
+		if (shortTerrainSpawnCount > 0) {
+			StartCoroutine (SpawnTerrainType (shortTerrainSpawnCount, shortTerrainShapes, shortSpawnBox));
+		}
+		if (midTerrainSpawnCount > 0) {
+			StartCoroutine (SpawnTerrainType (midTerrainSpawnCount, midTerrainShapes, midSpawnBox));
+		}
+		if (tallTerrainSpawnCount > 0) {
+			StartCoroutine (SpawnTerrainType (tallTerrainSpawnCount, tallTerrainShapes, tallSpawnBox));
+		}
+
 	}
 }
