d63a1c0d83d326864c291ece0903d8d5754113b8
Tylor Allison <tylor@foobox.com>
2017-02-16 19:57:21 -0800

    add gamesetup scene, and transition to game scene

---
+++ Assets/Scripts/GameSetup.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameSetup : MonoBehaviour {
+
+    public enum GameMode {
+        singlePlayer,
+        hostMultiPlayer,
+        joinMultiPlayer
+    }
+
+    // Public variables
+    // main UI panels
+    public GameObject startPanel;
+    public GameObject hostMultiPanel;
+    public GameObject joinMultiPanel;
+    GameObject[] allPanels;
+
+    // UI elements to pull data from
+    public Toggle hotSeatToggle;
+    public InputField hostMultiHostInput;
+    public InputField hostMultiPortInput;
+    public InputField joinMultiHostInput;
+    public InputField joinMultiPortInput;
+    public Text numPlayersDropdownLabel;
+
+    // game state
+    public GameMode gameMode;
+    public bool hotSeat;
+    public string host;
+    public int port;
+    public int numPlayers;
+
+	/// <summary>
+	/// setup convenience vars, and set this gameObject up so that it will not be destroyed between scene loads
+	/// </summary>
+    void Awake() {
+        allPanels = new GameObject[] { startPanel, hostMultiPanel, joinMultiPanel };
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+	/// <summary>
+	/// enable the starting panel, disable all others
+	/// </summary>
+    void Start() {
+        DisablePanels();
+        EnableStartPanel();
+    }
+
+	/// <summary>
+	/// disable all panels
+	/// </summary>
+    void DisablePanels() {
+        foreach (var panel in allPanels) {
+            panel.SetActive(false);
+        }
+    }
+
+	/// <summary>
+	/// enable the start panel
+	/// </summary>
+    public void EnableStartPanel() {
+        DisablePanels();
+        startPanel.SetActive(true);
+    }
+
+	/// <summary>
+	/// enable the host multiplayer panel
+	/// </summary>
+    public void EnableHostMultiPanel() {
+        DisablePanels();
+        gameMode = GameMode.hostMultiPlayer;
+        hostMultiPanel.SetActive(true);
+    }
+
+	/// <summary>
+	/// enable the join multiplayer panel
+	/// </summary>
+    public void EnableJoinMultiPanel() {
+        DisablePanels();
+        gameMode = GameMode.joinMultiPlayer;
+        joinMultiPanel.SetActive(true);
+    }
+
+	/// <summary>
+	/// handle host multiplayer start
+	/// </summary>
+    public void OnHostMultiStart() {
+        // gather game state
+        hotSeat = hotSeatToggle.isOn;
+        host = hostMultiHostInput.text;
+        port = Int32.Parse(hostMultiPortInput.text);
+        var numPlayersText = numPlayersDropdownLabel.GetComponent<Text>();
+        numPlayers = Int32.Parse(numPlayersText.text);
+
+        Debug.Log(String.Format("gameMode: {0} " +
+            "hotSeat: {1} " +
+            "host: {2} " +
+            "port: {3} " +
+            "numPlayers: {4} ",
+            gameMode, hotSeat, host, port, numPlayers));
+        StartGame();
+    }
+
+	/// <summary>
+	/// handle join multiplayer start
+	/// </summary>
+    public void OnJoinMultiStart() {
+        // gather game state
+        host = joinMultiHostInput.text;
+        port = Int32.Parse(joinMultiPortInput.text);
+
+        Debug.Log(String.Format("gameMode: {0} " +
+            "host: {1} " +
+            "port: {2} ",
+            gameMode, host, port));
+        StartGame();
+    }
+
+	/// <summary>
+	/// actually start the game by loading the Game scene
+	/// </summary>
+    public void StartGame() {
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+}
---
+++ Assets/Scripts/NetworkSetup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSetup : MonoBehaviour {
+    NetworkManager netManager;
+
+	/// <summary>
+	/// Upon start, setup the game
+	/// </summary>
+    void Start() {
+        netManager = NetworkManager.singleton;
+        // setup the game
+        SetupGame();
+    }
+
+	/// <summary>
+	/// The actual logic to setup the overall game play, based on values passed in GameSetup object
+	/// </summary>
+	void SetupGame () {
+        Debug.Log("NetworkSetup start");
+		// grab game setup info
+		var gameSetupGO = GameObject.Find("GameSetup");
+        Debug.Log("gameSetupGO: " + gameSetupGO);
+
+		// we were spawned from GameSetup scene...
+		if (gameSetupGO != null) {
+
+            var gameSetup = gameSetupGO.GetComponent<GameSetup>();
+            // set network manager state
+            netManager.networkPort = gameSetup.port;
+
+            // setup server
+            if (gameSetup.gameMode == GameSetup.GameMode.hostMultiPlayer) {
+                if (gameSetup.host != "localhost") {
+                    netManager.serverBindToIP = true;
+                    netManager.serverBindAddress = gameSetup.host;
+                }
+
+                Debug.Log("starting server");
+                netManager.StartHost();
+
+            // setup client
+            } else {
+                netManager.networkAddress = gameSetup.host;
+                // start the client
+                Debug.Log(String.Format("starting client: {0}:{1}", netManager.networkAddress, netManager.networkPort));
+                netManager.StartClient();
+            }
+
+		// debug mode
+		} else {
+
+            // add HUD GUI to network manager
+            netManager.gameObject.AddComponent<NetworkManagerHUD>();
+		}
+	}
+}
