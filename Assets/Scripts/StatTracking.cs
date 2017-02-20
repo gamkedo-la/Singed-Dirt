807b0dc4cef405aad58ee66a4788d1bf3b0006de
Jeremy Kenyon <lanthos@gmail.com>
2017-02-19 19:01:34 -0800

    SceneChanger script added, stats page added, buttons to move between game and stats page and hat selector page added

---
+++ Assets/Scripts/StatTracking.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatTracking {
+	public static float roundSeconds;
+	public static float currentRound;
+
+	public static void Reset(){
+		roundSeconds = 0;
+		currentRound = 0;
+	}
+}
--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -119,6 +119,9 @@ public class TurnManager : NetworkBehaviour {
 		} else {
 			gameOverText.enabled = false;
 		}
+		if (Input.GetKeyDown (KeyCode.N)) {
+			gameOverState = true;
+		}
 	}
 
 	int[] GetTurnOrder(Dictionary<int, TankController> tanks) {
