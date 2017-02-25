79dce23a043560524b183b5da4b5d220ba90f23d
Jeremy Kenyon <lanthos@gmail.com>
2017-02-24 20:22:25 -0800

    Created script to rotate model, created new ModelPosition with centered pivot point, create new buttons to allow for rotation of the model.

---
+++ Assets/Scripts/RotateModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateModel : MonoBehaviour {
+
+	public float rotateSpeed = 10.0f;
+	Transform modelPosition;
+
+	void Start(){
+		modelPosition = GameObject.Find ("ModelPosition").transform;
+	}
+	
+	public void RotateLeft(){
+		modelPosition.Rotate (Vector3.up * Time.deltaTime * rotateSpeed);
+
+	}
+
+	public void RotateRight(){
+		modelPosition.Rotate (Vector3.up * Time.deltaTime * rotateSpeed * -1);
+	}
+}
