5bcfad8d7a14b563472cf16dfa898a34d63b20b4
Trent <Xmedia101@yahoo.com>
2017-05-02 20:13:03 -0400

    Wake me up inside
    (Can't wake up)

---
+++ Assets/Scripts/CantWakeUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CantWakeUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake ()
+    {
+        Debug.Log("Wake me up inside");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
