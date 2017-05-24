9033ac96a7adf063a1a602c89bc86d11caee4d98
DesertJay <desertjaybirdie@gmail.com>
2017-05-24 13:22:05 -0700

    Nuke Prefab and Scene Setup
    WIP - the base components.

---
+++ Assets/Scripts/AsplosionScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AsplosionScript : MonoBehaviour {
+
+    public bool moveIt = false;
+    public Vector3 moveToPos;
+    public float startToMove,
+        timeToMove = 0;
+
+    public bool scaleIt = false;
+    public Vector3 scaleTo;
+    public float startToScale,
+        timeToScale = 0;
+
+    private NukeScript control;
+    private Vector3 startPos,
+        startScale;
+    private float stopMoveBy,
+        stopScaleBy,
+        moveTime = 0,
+        scaleTime = 0;
+
+    private void Awake() {
+        control = transform.GetComponentInParent<NukeScript>();
+        stopMoveBy = startToMove + timeToMove;
+        stopScaleBy = startToScale + timeToScale;
+    }
+
+    private void Start() {
+        startPos = transform.localPosition;
+        startScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (moveIt && control.timer >= startToMove) {
+            moveTime += Time.deltaTime / timeToMove;
+            transform.localPosition = Vector3.Lerp(startPos, moveToPos, moveTime);
+        }
+        if (control.timer >= stopMoveBy) moveIt = false;
+
+        if(scaleIt && control.timer >= startToScale) {
+            scaleTime += Time.deltaTime / timeToScale;
+            transform.localScale = Vector3.Lerp(startScale, scaleTo, scaleTime);
+        }
+        if (control.timer >= stopScaleBy) scaleIt = false;
+    }
+
+}
---
+++ Assets/Scripts/NukeScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class NukeScript : MonoBehaviour {
+
+    public float timer,
+        startSequenceAt = 1;
+    public Transform nukeCam;
+    public Bloom bloom;
+
+    private void Awake() {
+        timer = 0;
+    }
+
+    private void Update() {
+        timer += Time.deltaTime;
+        Debug.Log(timer.ToString());
+    }
+
+}
